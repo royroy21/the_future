8630183eb8621f5a4c22d432eb02502b18a7ff4e
roy <royhanley8@gmail.com>
2016-08-24 23:49:10 +0100

    Fix wsgi.py

--- the_future/the_future/wsgi.py
+++ the_future/the_future/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mc_notey.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_future.settings")
 
 application = get_wsgi_application()
