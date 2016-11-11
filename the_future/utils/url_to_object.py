93716224ded7ce2173a64c065eeee078438472b4
roy <royhanley8@gmail.com>
2016-11-11 19:06:24 +0000

    remove try/except

--- the_future/utils/url_to_object.py
+++ the_future/utils/url_to_object.py
@@ -2,10 +2,7 @@ from django.contrib.contenttypes.models import ContentType
 
 
 def url_to_object(obj_url):
-    try:
-        _, _, slug, id, *_ = obj_url.split('/')
-    except:
-        import ipdb; ipdb.set_trace()
+    _, _, slug, id, *_ = obj_url.split('/')
 
     ct = ContentType.objects.get(model=slug.replace('-', '').lower())
     return ct.model_class().objects.get(id=id)
